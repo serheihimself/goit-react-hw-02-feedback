7c5355fe15adae86d3efea19d88a16a228bb2204
serheihimself <sergm080@gmail.com>
2023-02-23 13:09:57 +0200

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,53 @@
+import React, { Component } from 'react';
+import Statistics from './Statisctics';
+import { BoxStats } from './Statistics.styles';
+
+class MainClass extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  handleState = ev => {
+    console.log(ev);
+    this.setState(prevstate => {
+      return (this.state.good = prevstate.good + 1);
+    });
+    this.setState(prevstate => {
+      return (this.state.neutral = prevstate.neutral + 1);
+    });
+    this.setState(prevstate => {
+      return (this.state.bad = prevstate.bad + 1);
+    });
+  };
+  render() {
+    return (
+      <BoxStats>
+        <h2>Please leave feedback</h2>
+        <button onClick={this.handleState}>Good</button>
+        <button onClick={this.handleState}>Neutral</button>
+        <button onClick={this.handleState}>Bad</button>
+        <h2>Statistics</h2>
+        <ul style={{ listStyle: 'none' }}>
+          <li>
+            <p>Good: {this.state.good}</p>
+          </li>
+          <li>
+            <p>Netral: {this.state.neutral}</p>
+          </li>
+          <li>
+            <p>Bad: {this.state.bad}</p>
+          </li>
+        </ul>
+      </BoxStats>
+    );
+  }
+}
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <MainClass />
     </div>
   );
 };
